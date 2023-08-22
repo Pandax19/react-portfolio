8c778659beb03ffaa17694f175bbd8626f8b3704
Pandax19 <ajwhitley@comcast.net>
2023-08-21 20:07:14 -0400

    added plant background and node script:

--- src/components/pages/nav/nav.jsx
+++ src/components/pages/nav/nav.jsx
@@ -1,34 +1,4 @@
 
-// import React from 'react';
-// import './nav.css';
-
-// export default function Nav() {
-//   return (
-//     <header>
-//         <nav className='navBar' role='navigation' aria-label='main nav'>
-//             <div className='navBar-brand'>
-//                 <a className='navBar-item' href='' ></a>
-
-//                 <button type="button" class="btn btn-primary"
-//                 >Home</button>
-//                 <button type="button" class="btn">About</button>
-//                 <button type="button" class="btn">Projects</button>
-//                 <button type="button" class="btn">Contact</button>
-//             </div>
-//         </nav>
-
-   
-//     {/* <Nav className="navbar">
-//       <ul className="nav-list">
-//         <li className="nav-item"><a href="/">Home</a></li>
-//         <li className="nav-item"><a href="/about">About</a></li>
-//         <li className="nav-item"><a href="/services">Projects</a></li>
-//         <li className="nav-item"><a href="/contact">Contact</a></li>
-//       </ul>
-//     </Nav> */}
-// </header>
-//   ); 
-// };
 
 import { Link } from 'react-router-dom';
 import { capitalizeFirstLetter } from '../../../utils/helpers';
